e4c330ce6ff0f1e35fc73e4cd72bc1ea642c0d70
Mac Anas <macanas@MacBook-Pro-de-Mac.local>
2025-10-16 16:34:21 +0200

    initialisation repo cda

---
+++ java/firstProject/src/main/java/com/insy2s/firstProject/model/User.java
@@ -0,0 +1,4 @@
+package com.insy2s.firstProject.model;
+
+public class User {
+}
---
+++ java/firstProject/src/test/java/com/insy2s/firstProject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insy2s.firstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
